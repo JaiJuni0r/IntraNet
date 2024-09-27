081c5931b04bb5ae100dbb7e56d35f9412fd9f8e
Jailson Junior <jailson_junior123@hotmail.com>
2024-09-27 15:20:11 -0300

    Alterações da navbar

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-  
-  document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
-
-    const hamburger = document.getElementById("hamburger");
-    const nav = document.getElementById("nav");
-    
-    hamburger.addEventListener("click", () => {
-        nav.classList.toggle("show");
-    });
-
 function goBack() {
       window.history.back()
- }
+    };
 
 function redirectTo(url) {
         window.location.href = url;
-    }    
+    };   
+
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+    
+hamburger.addEventListener('click', () => {
+        nav.classList.toggle('show');
+    });
